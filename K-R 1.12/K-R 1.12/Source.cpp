7ec1c4d7531c8c39a90c1add23f742447b7822fb
glemerenb <61454713+glemerenb@users.noreply.github.com>
2020-12-02 12:24:21 +0300

    Add files via upload

---
+++ K-R 1.12/K-R 1.12/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+//написать программу для вывода входного потока по одному слову в строке
+
+using namespace std;
+
+#define IN 1
+#define OUT 0
+
+int main()
+{
+	int character;
+	int state = OUT;
+
+	while ((character = getchar()) != EOF) {
+		if (character == ' ' || character == '\t')
+			state = OUT;
+		else if (state == OUT) {
+			putchar('\n');
+			state = IN;
+		}
+		putchar(character);
+	}
+}
